347e24e06e755bcdda94f5f61c906a29780bf017
ajaykumar <ajaykumar1196@gmail.com>
2021-02-21 22:26:43 +0530

    added create folder API

---
+++ src/main/java/com/cloudshare/cloudshareapi/dto/request/NewFolderRequest.java
@@ -0,0 +1,23 @@
+package com.cloudshare.cloudshareapi.dto.request;
+
+public class NewFolderRequest {
+
+    String folderName;
+    String destinationId;
+
+    public String getFolderName() {
+        return folderName;
+    }
+
+    public void setFolderName(String folderName) {
+        this.folderName = folderName;
+    }
+
+    public String getDestinationId() {
+        return destinationId;
+    }
+
+    public void setDestinationId(String destinationId) {
+        this.destinationId = destinationId;
+    }
+}
